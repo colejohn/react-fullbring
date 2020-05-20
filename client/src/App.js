ef0ebb3467ae87e5dc98208b58572d7b5358a0ac
beyond <nicolejohnn20@gmail.com>
2020-05-20 21:38:41 +0800

    added react graphql configuration and apollo setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+const SAMPLE_QUERY = gql`
+	{
+		users {
+			name
+		}
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	/* 
+    GRAPHQL QUERY
+  */
+	const { loading, error, data } = useQuery(SAMPLE_QUERY);
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :( </p>;
+	console.log(data);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>React GraphQL MongoDB and Apollo Boilerplate</p>
+				<a
+					className="App-link"
+					href="http://localhost:4000/playground"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					GraphQL Playground
+				</a>
+
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+
+				<a
+					className="App-link"
+					href="https://www.apollographql.com/docs/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn Apollo
+				</a>
+				<a
+					className="App-link"
+					href="https://docs.mongodb.com/manual/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn MongoDB
+				</a>
+				<a className="App-link" href="https://graphql.org/learn/" target="_blank" rel="noopener noreferrer">
+					Learn GraphQL
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const link = new HttpLink({
+	uri: 'http://localhost:4000/graphql',
+	credentials: 'same-origin'
+});
+
+const client = new ApolloClient({
+	link: ApolloLink.from([
+		onError(({ graphQLError, networkError }) => {
+			if (graphQLError) {
+				graphQLError.forEach(({ message, locations, path }) => {
+					console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`);
+				});
+			}
+			if (networkError) console.log(`[Network error]: ${networkError}`);
+		}),
+		link
+	]),
+	cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
